dd77fdc9480c3f051cace14c8ee53fb66179c34c
Richard Walroth <rwalroth89@gmail.com>
2020-03-27 20:57:05 -0700

    Incorporated a variable and cin
    In order to complete introductory concepts, added a variable.
    Does not yet do anything with choice.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main() {
+    int choice;
     cout << " ____________________________________________________________ " << endl;
     cout << "|                                                            |" << endl;
     cout << "|             Claudius: Praetorian Prefect                   |" << endl;
     cout << "|____________________________________________________________|" << endl;
+    cout << "\n\nEnter 1 to start the game, 2 to exit." << endl;
+    cin >> choice;
+    cout << "You have selected " << choice << ".";
     return 0;
 }
