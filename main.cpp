6658c9e0e4d941aa2491703d6ee4e067a8e96eac
Richard Walroth <rwalroth89@gmail.com>
2020-03-29 20:08:47 -0700

    Implemented constants and variables to set title

--- main.cpp
+++ main.cpp
@@ -3,13 +3,22 @@
 using namespace std;
 
 int main() {
+
+    const string frameTopOrBottom {" ------------------------------------------------------------ \n"};
+    const string frameMiddleEmpty {"|                                                            |\n"};
+
+    string frameMiddle0 {"|             Claudius: Praetorian Prefect                   |\n"};
+
+    cout << frameTopOrBottom;
+    cout << frameMiddleEmpty;
+    cout << frameMiddle0;
+    cout << frameMiddleEmpty;
+    cout << frameTopOrBottom;
+
+    cout << "\nEnter 1 to start the game, 2 to exit." << endl;
     int choice;
-    cout << " ____________________________________________________________ " << endl;
-    cout << "|                                                            |" << endl;
-    cout << "|             Claudius: Praetorian Prefect                   |" << endl;
-    cout << "|____________________________________________________________|" << endl;
-    cout << "\n\nEnter 1 to start the game, 2 to exit." << endl;
     cin >> choice;
     cout << "You have selected " << choice << ".";
+
     return 0;
 }
